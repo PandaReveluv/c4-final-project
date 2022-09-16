b54f189088ff2c98ec140527cb7afae8a1a04eee
PandaReveluv <dat19071998@gmail.com>
2022-09-16 17:53:46 +0700

    implement toDoForUsers method

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -6,5 +6,34 @@ import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
 import * as createError from 'http-errors'
+import * as AWS from 'aws-sdk';
+
+const dynamoDBClient = new AWS.DynamoDB.DocumentClient
 
 // TODO: Implement businessLogic
+export async function getTodosForUser(userId: string) {
+    let result:TodoItem[]
+    const items = await dynamoDBClient
+    .query({
+      TableName: 'Items',
+      IndexName: 'index-name',
+      KeyConditionExpression: 'paritionKey = :paritionKey',
+      ExpressionAttributeValues: {
+        ':paritionKey': userId
+      }
+    })
+    .promise()
+
+    items.Items.forEach(item => {
+        let toDoItem: TodoItem
+        toDoItem.userId = item.userId
+        toDoItem.todoId = item.todoId
+        toDoItem.createdAt = item.createdAt
+        toDoItem.name = item.name
+        toDoItem.dueDate = item.dueDate
+        toDoItem.done = item.done
+        toDoItem.attachmentUrl = item.attachmentUrl
+        result.push()
+    })
+    return result
+}
