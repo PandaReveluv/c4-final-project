c820c2426e6b05f46d9ba8605abf2908863a2f13
PandaReveluv <dat19071998@gmail.com>
2022-10-01 13:42:00 +0700

    add Unit Test for Todos

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    transform: {
+        '^.+\\.ts?$': 'ts-jest'
+    },
+    testEnvironment: 'node',
+    testRegex: '/tests/.*\\.(tests|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
